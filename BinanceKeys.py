cd81ca60e7bec6cc5d0200bd766869247493a59e
tirajetw <tirajetw@gmail.com>
2019-09-04 16:06:41 +0700

    First commit

---
+++ BinanceKeys.py
@@ -0,0 +1,3 @@
+BinanceKey1 = {'api_key': 'f7xQybTWPrY20G1NYoW9AE8MF1C5qPk0lS9l7sWUSgWeUaWYBKYVQJLBySnYtkmF',
+    'api_secret':'E8jtxPvGiztGlgN9UjSAiO7eNZqI8kpFDYwMJROmtJXf5vonRF02IdCW9yE4Ow9t'}
+	
